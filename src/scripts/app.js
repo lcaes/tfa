47b46636378434d67af04e0ba0b6a0f14fff6a86
lcaes <caeslyn12@gmail.com>
2026-05-14 01:04:27 +0200

    fix hearder +animation  btn nav blog

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,9 @@
 const blogBurger = document.querySelector('.blog__burger');
 const blogNav = document.querySelector('.blog__nav');
-const scroll = window.scrollY;
-console.log(scroll);
+const lunaInput = document.querySelector('.luans__input')
+let lastScroll=0;
+const widthScreen = screen.width
+console.log(widthScreen)
 
 blogBurger.addEventListener('click', opennav); 
 
@@ -11,4 +13,15 @@ function opennav() {
 }
     function closeNavigation() {
     blogNav.classList.remove('blog__nav--active');
+}
+
+window.addEventListener('scroll',showBTN);
+function showBTN(e){
+    const scrollCurrent = window.scrollY;
+    if(scrollCurrent < lastScroll && widthScreen < 1024){
+    blogBurger.classList.add("--show")}
+    else{
+        blogBurger.classList.remove("--show")
+    }
+    lastScroll = scrollCurrent
 }
